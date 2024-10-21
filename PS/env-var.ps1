1c5fe36d993ba43ca1f3756c58c9e3a9a55ca7d0
Jordan Sumner <144553157+JordanSum@users.noreply.github.com>
2024-10-21 13:00:32 -0700

    minor adjustment

--- PS/env-var.ps1
+++ PS/env-var.ps1
@@ -1,6 +1,6 @@
-$VMname = "test-vm"
-$RGname = "test-rg"
-$NSGname = "test-nsg"
-$KVSecretName = "test-key"
-$VMimage = "Ubuntu2204"
-$VMsize = "Standard_B1s"
+$VMname = "<your-vm-name>"
+$RGname = "<your-rg-name>"
+$NSGname = "<your-nsg-name>"
+$KVSecretName = "<your-secret-name>"
+$VMimage = "Ubuntu2204" # Change this to the image you want to use
+$VMsize = "Standard_B1s" # Change this to the VM size you want to use
